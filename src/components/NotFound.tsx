7e4c118fab5e313c687d194b7d6cb6de56dcc42f
Ganesh Kantimahanthi <60934037+knganesh97@users.noreply.github.com>
2025-09-25 20:03:26 +0530

    added not found component

---
+++ src/components/NotFound.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <div className="p-8 text-center text-xl font-semibold">
+      Challenge Not Found
+    </div>
+  );
+}
