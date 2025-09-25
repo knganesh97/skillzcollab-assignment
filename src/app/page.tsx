c3e239fa02d7f9d6622efcf6fad294fdb8146548
Ganesh Kantimahanthi <60934037+knganesh97@users.noreply.github.com>
2025-09-25 15:29:12 +0530

    redirect home page to challenges list page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <div className="text-2xl text-center pt-20">
-      The gradient layout is working!
-    </div>
-  );
+  redirect("/challenges");
+  return null;
 }
